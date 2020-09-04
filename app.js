7674c288b7ac1ec10333160078e93ce069182104
JasanCarsley <jjcars@comcast.net>
2020-09-03 20:17:31 -0700

    first revision calculator

---
+++ app.js
@@ -0,0 +1,21 @@
+// import functions and grab DOM elements
+
+// initialize state
+
+// set event listeners to update state and DOM
+sumButton.addEventListener('click', () => {
+    
+    console.log(sumInput1);
+    const value1 = sumInput1.valueAsNumber;
+    const value2 = sumInput2.valueAsNumber;
+
+
+    const sum = value1 + value2;
+
+    console.log(sum)
+
+
+});
+
+
+
