bdb3e3df4758db6bde2ca1fa035e8955501f83b6
JasanCarsley <jjcars@comcast.net>
2020-09-23 20:27:45 -0700

    added DOM elements, event listeners

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // import functions and grab DOM elements
+const sumInput1 = document.getElementById("sum-input-1");
+const sumInput2 = document.getElementById("sum-input-2");
+const sumButton = document.getElementById("sum-button");
 
 // initialize state
 
 // set event listeners to update state and DOM
 sumButton.addEventListener('click', () => {
-    
-    console.log(sumInput1);
-    const value1 = sumInput1.valueAsNumber;
-    const value2 = sumInput2.valueAsNumber;
+    const value1 = sumInput1.value1;
+    const value2 = sumInput2.value2;
 
+    console.log(value1, value2);
 
-    const sum = value1 + value2;
+    const value1AsNumber = Number(value1);
+    const value2AsNumber = Number(value2);
+
+    console.log(typeof value1AsNumber, typeof value2AsNumber);
+
+    const sum = value1AsNumber + value2AsNumber;
 
     console.log(sum)
 
-
+    addResults.textContent = sum;
 });
-
-
-
