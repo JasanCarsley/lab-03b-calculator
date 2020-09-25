3d42a9809c06b4eea0e10d313438c5dc872e9c92
JasanCarsley <jjcars@comcast.net>
2020-09-24 18:44:47 -0700

    added tests, imports, exports

---
+++ math.js
@@ -0,0 +1,15 @@
+export function addNumbers(inPut1, inPut2) {
+    return inPut1 + inPut2
+}
+
+export function subNumbers(inPut1, inPut2) {
+    return inPut1 - inPut2
+}
+
+export function multNumbers(inPut1, inPut2) {
+    return inPut1 * inPut2
+}
+
+export function divNumbers(inPut1, inPut2) {
+    return inPut1 / inPut2
+} 
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,57 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import {addNumbers} from '../math.js';
+import {subNumbers} from '../math.js';
+import {multNumbers} from '../math.js';
+import {divNumbers} from '../math.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('test should add two numbers together', (expect) => {
+    const input1 = 2
+    const input2 = 2
+    
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = addNumbers(input1, input2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+
+test('test should subtract two numbers', (expect) => {
+    const input1 = 2
+    const input2 = 2
+    
+    const expected = 0;
+ 
+    const actual = subNumbers(input1, input2);
+
+    expect.equal(actual, expected);
+});
+
+test('test should multiply two numbers', (expect) => {
+    const input1 = 2
+    const input2 = 3
+
+    const expected = 6
+
+    const actual = multNumbers(input1, input2);
+
+    expect.equal(actual, expected)
+});
+
+test('test should divide two numbers', (expect) => {
+    const input1 = 6
+    const input2 = 3
+
+    const expected = 2
+
+    const actual = divNumbers(input1, input2);
+
+    expect.equal(actual, expected)
 });
